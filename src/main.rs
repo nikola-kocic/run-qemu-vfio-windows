3170738df3141b513b670a3d92db442e8fbdadfa
Nikola Kocic <nikolakocic@gmail.com>
2018-12-19 15:49:11 +0100

    Add undoable_command module

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,60 @@
 use std::process::Command;
 
-struct UndoableCommand<'a> {
-    cmd: &'a mut Command,
-    undo: &'a mut Command,
-    cmd_executed: bool,
-}
+mod undoable_command {
+    use std::process::Command;
+    pub struct UndoableCommand<'a> {
+        cmd: &'a mut Command,
+        undo: &'a mut Command,
+        cmd_executed: bool,
+    }
 
-fn verbose_execute_cmd(cmd: &mut Command) -> Result<(), std::process::ExitStatus> {
-    println!("running: {:?}", cmd);
-    let output = cmd.output().unwrap();
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    if !stdout.is_empty() {
-        println!("  stdout: {}", stdout);
-    }
-    let stderr = String::from_utf8_lossy(&output.stderr);
-    if !stderr.is_empty() {
-        println!("  stderr: {}", stderr);
-    }
-    if !output.status.success() {
-        Err(output.status)
-    } else {
-        Ok(())
-    }
-}
-
-impl<'a> UndoableCommand<'a> {
-    fn new(cmd: &'a mut Command, undo: &'a mut Command) -> UndoableCommand<'a> {
-        UndoableCommand {
-            cmd,
-            undo,
-            cmd_executed: false,
+    fn verbose_execute_cmd(cmd: &mut Command) -> Result<(), std::process::ExitStatus> {
+        println!("running: {:?}", cmd);
+        let output = cmd.output().unwrap();
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        if !stdout.is_empty() {
+            println!("  stdout: {}", stdout);
+        }
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        if !stderr.is_empty() {
+            println!("  stderr: {}", stderr);
+        }
+        if !output.status.success() {
+            Err(output.status)
+        } else {
+            Ok(())
         }
     }
 
-    fn run(&mut self) -> Result<(), std::process::ExitStatus> {
-        verbose_execute_cmd(&mut self.cmd).map(|v| {
-            self.cmd_executed = true;
-            v
-        })
-    }
-}
+    impl<'a> UndoableCommand<'a> {
+        pub fn new(cmd: &'a mut Command, undo: &'a mut Command) -> UndoableCommand<'a> {
+            UndoableCommand {
+                cmd,
+                undo,
+                cmd_executed: false,
+            }
+        }
 
-impl<'a> Drop for UndoableCommand<'a> {
-    fn drop(&mut self) {
-        if self.cmd_executed {
-            verbose_execute_cmd(&mut self.undo).unwrap_or_else(|e| {
-                eprintln!("Command exited with: {}", e);
-            });
+        pub fn run(&mut self) -> Result<(), std::process::ExitStatus> {
+            verbose_execute_cmd(&mut self.cmd).map(|v| {
+                self.cmd_executed = true;
+                v
+            })
+        }
+    }
+
+    impl<'a> Drop for UndoableCommand<'a> {
+        fn drop(&mut self) {
+            if self.cmd_executed {
+                verbose_execute_cmd(&mut self.undo).unwrap_or_else(|e| {
+                    eprintln!("Command exited with: {}", e);
+                });
+            }
         }
     }
 }
+
+use crate::undoable_command::UndoableCommand;
 
 fn run_app() -> Result<(), std::process::ExitStatus> {
     UndoableCommand::new(
